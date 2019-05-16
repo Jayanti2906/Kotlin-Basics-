2c24c307ac8064ef9653fd30144d088ee3cc6fc8
jayanti singh <jayanti.singh@SLP160137-Jayanthi.local>
2019-05-16 13:11:20 +0530

    Normal kotlin function vs function as expression

---
+++ function-vs-function_as_exp.kt
@@ -0,0 +1,59 @@
+ //Funtion in Kotlin
+/*
+ fun main()
+{
+    println("Enter the two numbers ")
+     var x = Integer.valueOf(readLine())
+    var y = Integer.valueOf(readLine())
+    println("The greater is ${max(x,y)}")
+
+
+}
+fun max (a: Int, b : Int):Int
+   {
+        if (a>b)
+        {
+            println("Finding greater ...")
+            return a
+
+        }
+        else
+        {
+            println("Finding greater ...")
+            return b
+
+        }
+    }
+
+*/
+
+//We can also write the above Function as Expression :--
+//This helps in code optimization
+
+
+
+// /*
+
+fun main()
+
+{
+    println("Enter the two numbers ")
+    var x = Integer.valueOf(readLine())
+    var y = Integer.valueOf(readLine())
+
+    println(" The greater number is ${max(x,y)}")
+}
+//the entire function is written as an expression
+
+fun max (a: Int, b : Int):Int =            // NOTE : here we have used the equal to sign
+        if (a>b)
+        {
+            println("finding greater...")
+            a
+        }
+        else {
+            println("finding greater...")
+            b
+        }
+
+// */
